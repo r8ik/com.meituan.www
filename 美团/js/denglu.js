fb0f803d4e81091678773c3a4c87eea4fca7292a
r8ik <1776135749@qq.com>
2020-04-29 07:54:04 +0800

    1

---
+++ 美团/js/denglu.js
@@ -0,0 +1,21 @@
+$('.center-tel input').focus(function(){
+  $('.center-tel').css({
+    'border': '1px solid #ffc300'
+  })
+})
+$('.center-tel input').blur(function(){
+  $('.center-tel').css({
+    'border': '1px solid #aaaaaa'
+  })
+})
+
+$('.center-pass input').focus(function(){
+  $('.center-pass').css({
+    'border': '1px solid #ffc300'
+  })
+})
+$('.center-pass input').blur(function(){
+  $('.center-pass').css({
+    'border': '1px solid #aaaaaa'
+  })
+})
