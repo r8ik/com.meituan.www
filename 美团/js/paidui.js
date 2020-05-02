744a5aa9f45a8db15f2c1bcefdcabdfde75c4543
r8ik <1776135749@qq.com>
2020-05-02 19:43:39 +0800

    1

--- 美团/js/paidui.js
+++ 美团/js/paidui.js
@@ -6,17 +6,33 @@
 // 免费试用按键
 // console.log($(".map-free"))
 
-$(".map-free").click(function(){
+$(".map-free").click(function () {
   $('.down i').fadeToggle(200)
   $('.down').slideToggle(500)
 })
-$(' .down li').hover(function(){
+$(' .down li').hover(function () {
   $(this).animate({
     'margin-top': '12px'
-  },200)
-},function(){
+  }, 200)
+}, function () {
   $(this).animate({
     'margin-top': '25px'
-  },200)
+  }, 200)
 })
 
+// 硬件下面的盒子
+console.log($('.ying-li'))
+$('.ying-li').mouseenter(function () {
+  $(this).find('i').css({
+
+    'background': '#06c1ae'
+  })
+  $(this).siblings().find('i').css({
+    'background': 'transparent'
+  })
+  let index=$(this).index()
+  // console.log($(this).index())
+  // console.log($('.ying-box'))
+  $('.ying-box').eq(index).addClass('active')
+  $('.ying-box').eq(index).siblings().removeClass('active')
+})
